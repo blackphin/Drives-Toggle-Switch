2480456d08f159c08f7b157072b9a2423bfe691d
Shivansh Goel <44195921+blackphin@users.noreply.github.com>
2020-07-22 01:02:24 +0530

    Create Drives-Toggle-Switch.bat

---
+++ Drives-Toggle-Switch.bat
@@ -0,0 +1,13 @@
+@echo off
+tasklist /FI "IMAGENAME eq OneDrive.exe" 2>NUL | find /I /N "OneDrive.exe">NUL
+if "%ERRORLEVEL%"=="0" goto Process_Found
+:Process_NotFound
+start "" "C:\Program Files\Google\Drive\googledrivesync.exe"
+start "" "C:\Users\shiva\AppData\Local\Microsoft\OneDrive\OneDrive.exe"
+timeout 2
+wscript "D:\OneDrive\My Programs\Drives\sendkey.vbs"
+goto END
+:Process_Found
+taskkill /IM googledrivesync.exe /T /F
+taskkill /IM OneDrive.exe /T /F
+:END
